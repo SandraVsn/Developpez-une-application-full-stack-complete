fd622efcafa36dbc9d359c75f32f78d2cc49b48b
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-01-22 20:27:58 +0100

    use tailwind css

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
 
-  ngOnInit(): void {}
-
-  start() {
-    alert('Commencez par lire le README et à vous de jouer !');
-  }
 }
