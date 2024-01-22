4af400df469076a658437853ce464a6cf74b3b43
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-01-22 20:21:32 +0100

    comment repository

---
+++ back/src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.mddapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.openclassrooms.mddapi.model.Comment;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+    public Iterable<Comment> findAllByPostId(Long postId);
+
+}
