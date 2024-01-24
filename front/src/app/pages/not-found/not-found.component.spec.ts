47f0ed3a0611295ec8349ffa7b6dc4c5b9b5472e
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-01-24 11:36:30 +0100

    create not found component

--- front/src/app/pages/not-found/not-found.component.spec.ts
+++ front/src/app/pages/not-found/not-found.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MeComponent } from './me.component';
+import { NotFoundComponent } from './not-found.component';
 
-describe('MeComponent', () => {
-  let component: MeComponent;
-  let fixture: ComponentFixture<MeComponent>;
+describe('NotFoundComponent', () => {
+  let component: NotFoundComponent;
+  let fixture: ComponentFixture<NotFoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MeComponent]
+      imports: [NotFoundComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MeComponent);
+    fixture = TestBed.createComponent(NotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
