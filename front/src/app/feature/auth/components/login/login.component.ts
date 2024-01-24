e4109075806d1fdbc0aed3749d9e3fa2161f95a9
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-01-24 20:20:58 +0100

    fix: redirects

--- front/src/app/feature/auth/components/login/login.component.ts
+++ front/src/app/feature/auth/components/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent {
     // this.authService.login(loginRequest).subscribe({
     //   next: (response: SessionInformation) => {
     //     this.sessionService.logIn(response);
-    //     this.router.navigate(['/sessions']);
+      this.router.navigate(['/posts']);
     //   },
     //   error: error => this.onError = true,
     // });
