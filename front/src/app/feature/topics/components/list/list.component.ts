d25c516edc733a3333705f3161c0096318e676a7
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-02-02 09:14:08 +0100

    topic list component (ts)

--- front/src/app/feature/topics/components/list/list.component.ts
+++ front/src/app/feature/topics/components/list/list.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../../components/header/header.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { TopicApiService } from '../../services/topic-api.service';
+import { Topic } from '../../interfaces/topic.interface';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MatIconModule, MatButtonModule],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
 })
 export class ListComponent {
+
+  constructor(private topicApiService: TopicApiService){}
+
+  public topics: Topic[] = [];
+  public loaded = false;
+  public onError = false;
+
+  ngOnInit(): void {
+    this.topicApiService.getAll().subscribe({
+      next: (topics) => {
+        this.topics = topics;
+        this.loaded = true;
+      },
+      error: () => this.onError = true,
+    });
+  }
 
 }
