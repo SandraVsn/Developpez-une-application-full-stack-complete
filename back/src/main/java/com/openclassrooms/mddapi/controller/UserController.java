bf4e25cd3a43e7b379c8efcfbb2932b473129fb4
Sandra VOISIN <sandra.voisin92@gmail.com>
2024-02-02 15:07:37 +0100

    add update user dto & route

--- back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/UserController.java
@@ -1,33 +1,45 @@
 package com.openclassrooms.mddapi.controller;
 
+import java.security.Principal;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.mddapi.dto.CreateUserDto;
+import com.openclassrooms.mddapi.dto.UpdateUserDto;
 import com.openclassrooms.mddapi.dto.UserDto;
 import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.service.AuthService;
 import com.openclassrooms.mddapi.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = "${apiPrefix}/user")
 @Tag(name = "User", description = "API for User CRUD operations")
 public class UserController {
 
     private UserService userService;
+    private AuthService authService;
     private ModelMapper modelMapper;
 
     @Autowired
-    public UserController(UserService userService, ModelMapper modelMapper) {
+    public UserController(UserService userService, AuthService authService, ModelMapper modelMapper) {
         this.userService = userService;
+        this.authService = authService;
         this.modelMapper = modelMapper;
     }
 
@@ -46,6 +58,21 @@ public class UserController {
             return ResponseEntity.badRequest().build();
         }
     }
+    
+    @PutMapping()
+    public ResponseEntity<?> update(Principal principal, @RequestBody UpdateUserDto updateUserDto) {
+        try {
+            User user = authService.getMe(principal.getName());
+            user.setEmail(updateUserDto.getEmail());
+            user.setUserName(updateUserDto.getUserName());
+            User updatedUser = this.userService.saveUser(user);
+
+            return ResponseEntity.ok().body(modelMapper.map(updatedUser, UserDto.class));
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
     @PostMapping("subscribe/{topicId}")
     public ResponseEntity<?> subscribe(Principal principal, @PathVariable("topicId") String topicId) {
         try {
