7eb41ed5192e7e3b424013a50a632b391a5680be
SandraVsn <sandra.voisin92@gmail.com>
2024-02-15 21:11:28 +0100

    remove useless keys from comment dto

--- back/src/main/java/com/openclassrooms/mddapi/dto/CommentDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/CommentDto.java
@@ -1,25 +1,12 @@
 package com.openclassrooms.mddapi.dto;
 
-import java.time.LocalDateTime;
-
-import com.openclassrooms.mddapi.model.Post;
-import com.openclassrooms.mddapi.model.User;
-
 import lombok.Data;
 
 @Data
 public class CommentDto {
 
-    private Long id;
-
     private String content;
 
-    private User user;
-
-    private Post post;
-
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
+    private String userName;
 
 }
