16c65abeb48ceb353867d8ee5abfe27ef94692c4
SandraVsn <sandra.voisin92@gmail.com>
2024-02-15 14:52:48 +0100

    feat: 404 not found

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
-  HTTP_INTERCEPTORS,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
