fea1c77e21054d3e777754524849713964ecda50
SandraVsn <sandra.voisin92@gmail.com>
2024-02-23 12:27:37 +0100

    fix : use async pipe instead of subscribe ( Topics list)

--- front/src/app/feature/topics/components/list/list.component.ts
+++ front/src/app/feature/topics/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../../../components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,51 +6,66 @@ import { TopicApiService } from '../../services/topic-api.service';
 import { Topic } from '../../interfaces/topic.interface';
 import { UserService } from '../../../../services/user.service';
 import { AuthService } from '../../../auth/services/auth.service';
+import { Observable, catchError, of, take } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [HeaderComponent, MatIconModule, MatButtonModule],
+  imports: [CommonModule, HeaderComponent, MatIconModule, MatButtonModule],
   templateUrl: './list.component.html',
 })
 export class ListComponent {
+  private readonly topicApiService = inject(TopicApiService);
+  private readonly userService = inject(UserService);
+  private readonly authService = inject(AuthService);
 
-  constructor(private topicApiService: TopicApiService, private userService: UserService, private authService: AuthService){}
-
-  public topics: Topic[] = []
-  public userTopics: Topic[] = []
-  public loaded = false;
+  public topics$!: Observable<Topic[] | undefined>;
+  public userTopics: Topic[] = [];
   public onError = false;
 
   ngOnInit(): void {
-    this.topicApiService.getAll().subscribe({
-      next: (topics) => {
-        this.topics = topics;
-        this.getUserTopics()
-        this.loaded = true;
-      },
-      error: () => this.onError = true,
-    });
+    this.topics$ = this.topicApiService.getAll().pipe(
+      catchError(() => {
+        this.onError = true;
+        return of(undefined);
+      })
+    );
+    this.getUserTopics();
   }
 
-  subscribe(topicId: number){
-    this.userService.subscribeToTopic(topicId).subscribe({
-      next: () => this.getUserTopics(), 
-      error: () => this.onError = true
-    })
+  subscribe(topicId: number) {
+    this.userService
+      .subscribeToTopic(topicId)
+      .pipe(take(1))
+      .subscribe({
+        next: () => this.getUserTopics(),
+        error: () => (this.onError = true),
+      });
   }
 
-  getUserTopics(){
-    this.authService.me().subscribe({
-      next: (user) => {
-        this.userTopics = user.topics;
-      },
-      error: () => this.onError = true,
-    });
+  getUserTopics() {
+    this.authService
+      .me()
+      .pipe(
+        take(1),
+        catchError(() => {
+          this.onError = true;
+          return of(undefined);
+        })
+      )
+      .subscribe({
+        next: (user) => {
+          if (user) {
+            this.userTopics = user?.topics;
+          } else {
+            throw new Error('User not found');
+          }
+        },
+      });
   }
 
-  disabled(topicId: number){
-    return this.userTopics.find(topic => topic.id === topicId)
+  disabled(topicId: number) {
+    return this.userTopics.find((topic) => topic.id === topicId);
   }
-
 }
