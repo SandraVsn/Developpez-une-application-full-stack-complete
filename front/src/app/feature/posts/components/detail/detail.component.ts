08b32122f33a6a96fa8b0cb7ef5a463bec7ef13b
SandraVsn <sandra.voisin92@gmail.com>
2024-02-23 11:37:54 +0100

    fix : use async pipe instead of subscribe (Details)

--- front/src/app/feature/posts/components/detail/detail.component.ts
+++ front/src/app/feature/posts/components/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../../../components/header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,15 +13,17 @@ import {
 import { ActivatedRoute, Router } from '@angular/router';
 import { PostApiService } from '../../services/post-api.service';
 import { CommentApiService } from '../../services/comment-api.service';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { Post } from '../../interfaces/post.interface';
 import { MatIconModule } from '@angular/material/icon';
 import { Comment } from '../../interfaces/comment.interface';
+import { Observable, catchError, map, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     MatButtonModule,
     MatFormFieldModule,
@@ -33,60 +35,53 @@ import { Comment } from '../../interfaces/comment.interface';
   templateUrl: './detail.component.html',
 })
 export class DetailComponent {
-  constructor(
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private postApiService: PostApiService,
-    private commentApiService: CommentApiService,
-    private location: Location
-  ) {}
+  private readonly fb = inject(FormBuilder);
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly postApiService = inject(PostApiService);
+  private readonly commentApiService = inject(CommentApiService);
+  private readonly location = inject(Location);
 
-  public loaded = false;
   public onError = false;
-  public post!: Post;
-  public comments: Comment[] = [];
+  public post$!: Observable<Post | undefined>;
+  public comments$!: Observable<Comment[] | undefined>;
 
   public form: FormGroup = this.fb.group({
     content: ['', [Validators.required, Validators.minLength(3)]],
   });
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe({
-      next: (params) => {
-        const id = params.get('id');
-        if (id) {
-          this.postApiService.getOne(id).subscribe({
-            next: (post) => {
-              this.post = post;
-              this.loaded = true;
-            },
-          });
-          this.commentApiService.getAllByPostId(id).subscribe({
-            next: (comments) => {
-              this.comments = comments;
-            },
-            error: () => (this.onError = true),
-          });
+    const id = this.route.snapshot.params['id'];
+    this.post$ = this.postApiService.getOne(id).pipe(
+      map((post) => {
+        if (post) {
+          return post;
         } else {
           this.router.navigate(['/posts']);
+          throw new Error('Post not found');
         }
-      },
-      error: () => (this.onError = true),
-    });
+      }),
+      catchError(() => {
+        this.onError = true;
+        return of(undefined);
+      })
+    );
+    this.comments$ = this.commentApiService.getAllByPostId(id).pipe(
+      catchError(() => {
+        this.onError = true;
+        return of(undefined);
+      })
+    );
   }
 
-  submit() {
+  submit(postId: number) {
+    console.log(postId)
     const commentDto = {
       content: this.form.value.content,
-      postId: this.post.id,
+      postId,
     };
-    this.commentApiService.create(commentDto).subscribe({
-      next: (comments) => {
-        this.comments = comments;
-        this.form.reset();
-      },
-    });
+    this.comments$ = this.commentApiService.create(commentDto).pipe(take(1));
+    this.form.reset();
   }
 
   goBack() {
